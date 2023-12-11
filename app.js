991f8cb9b6bdf80c636b40e095354a5d87cffb80
Leeladhar Suthar <lldhrsuthar@gmail.com>
2023-12-12 00:13:47 +0530

    Added listing route

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const Listing = require("./models/listing.js");
 
 const app = express();
 
-app.set('view-engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", 'ejs');
+app.set("views", path.join(__dirname, "/views"));
 
 const port = 8080;
 const MONGO_URL = 'mongodb://127.0.0.1:27017/projectOne';
@@ -25,13 +25,14 @@ app.listen(port, () => {
     console.log("Server listening at port 8080");
 })
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
     res.send("Home page");
 })
 
-app.get('/listing', async (req, res)=>{
-    const listing = await Listing.find();
-    res.render('listing.ejs', {listing});
+app.get("/listing", async (req, res)=>{
+    const listing = await Listing.find({});
+    res.render("./listing/index.ejs", {listing});
+
 })
 
 // app.get("/listing", async (req, res) => {
